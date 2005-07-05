e069946f7162b3cf5477724d9ccf4175e91d5c26
Stephan Borg <wolff_borg@users.sourceforge.net>
2005-07-05 12:15:01 +0000

    Changed *_PKG_DIR to *_PKG_NAME in all schema_inc.php

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -205,7 +205,7 @@ $tables = array(
 global $gBitInstaller;
 
 foreach( array_keys( $tables ) AS $tableName ) {
-	$gBitInstaller->registerSchemaTable( USERS_PKG_DIR, $tableName, $tables[$tableName], TRUE );
+	$gBitInstaller->registerSchemaTable( USERS_PKG_NAME, $tableName, $tables[$tableName], TRUE );
 }
 
 $indices = array (
@@ -227,7 +227,7 @@ $indices = array (
 
 
 
-$gBitInstaller->registerSchemaIndexes( USERS_PKG_DIR, $indices );
+$gBitInstaller->registerSchemaIndexes( USERS_PKG_NAME, $indices );
 
 $gBitInstaller->registerPackageInfo( USERS_PKG_NAME, array(
 	'description' => "The users package contains all user information and gives you the possiblity to assign permissions to groups of users.",
@@ -242,7 +242,7 @@ $sequences = array (
 	'users_users_user_id_seq' => array( 'start' => 2 ),
 	'users_groups_id_seq' => array( 'start' => 4 )
 );
-$gBitInstaller->registerSchemaSequences( USERS_PKG_DIR, $sequences );
+$gBitInstaller->registerSchemaSequences( USERS_PKG_NAME, $sequences );
 
 // ### Default MenuOptions
 $gBitInstaller->registerMenuOptions( USERS_PKG_NAME, array(
