36fe765d578888117189916904dce056d514a2c2
drewslater <drewslater@users.sourceforge.net>
2005-08-04 20:25:22 +0000

    test for checking if user is logged in was BitUser::isValid. Changed ti isRegistered()

--- my_images.php
+++ my_images.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/my_images.php,v 1.1.1.1.2.2 2005/07/26 15:50:30 drewslater Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/my_images.php,v 1.1.1.1.2.3 2005/08/04 20:25:22 drewslater Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: my_images.php,v 1.1.1.1.2.2 2005/07/26 15:50:30 drewslater Exp $
+ * $Id: my_images.php,v 1.1.1.1.2.3 2005/08/04 20:25:22 drewslater Exp $
  * @package users
  * @subpackage functions
  */
@@ -26,7 +26,7 @@ if ($feature_userPreferences != 'y') {
 	$gBitSystem->display( 'error.tpl' );
 	die;
 }
-if (!$gBitUser->isValid()) {
+if (!$gBitUser->isRegistered()) {
 	$gBitSmarty->assign('msg', tra("You are not logged in"));
 	$gBitSystem->display( 'error.tpl' );
 	die;
