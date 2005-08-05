1121700bb19e742d4cff68ca135f4aaa388d69f4
Lester Caine <lester@lsces.co.uk>
2005-08-05 16:15:46 +0000

    Ignore update of session database entry if no database connection active

--- BitUser.php
+++ BitUser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.2.2.28 2005/08/04 19:26:50 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.2.2.29 2005/08/05 16:15:46 lsces Exp $
  *
  * Lib for user administration, groups and permissions
  * This lib uses pear so the constructor requieres
@@ -12,7 +12,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitUser.php,v 1.2.2.28 2005/08/04 19:26:50 lsces Exp $
+ * $Id: BitUser.php,v 1.2.2.29 2005/08/05 16:15:46 lsces Exp $
  * @package users
  */
 
@@ -40,7 +40,7 @@ define("ACCOUNT_DISABLED", -6);
  * Class that holds all information for a given user
  *
  * @author   spider <spider@steelsun.com>
- * @version  $Revision: 1.2.2.28 $
+ * @version  $Revision: 1.2.2.29 $
  * @package  users
  * @subpackage  BitUser
  */
@@ -236,6 +236,7 @@ class BitUser extends LibertyAttachable {
 
 
 	function updateSession( $pSessionId ) {
+		if ( empty($this->mDb->mDb) ) return true; 
 		$now = date("U");
 		$oldy = $now - (5 * 60);
 		$bindVars = array( $now, $pSessionId );
