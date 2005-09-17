20e943983cbc70bfdfed18308377f6f3bf9dd6b6
Max Kremmel <xing@synapse.plus.com>
2005-09-17 16:38:24 +0000

    bug 1289242: redirect user to homepage after image upload

--- my_images.php
+++ my_images.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/my_images.php,v 1.1.1.1.2.3 2005/08/04 20:25:22 drewslater Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/my_images.php,v 1.1.1.1.2.4 2005/09/17 16:38:24 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: my_images.php,v 1.1.1.1.2.3 2005/08/04 20:25:22 drewslater Exp $
+ * $Id: my_images.php,v 1.1.1.1.2.4 2005/09/17 16:38:24 squareing Exp $
  * @package users
  * @subpackage functions
  */
@@ -52,6 +52,8 @@ $_REQUEST["user_id"]=$gQueryUser->mUserId;
 // Upload avatar is processed here
 if( !empty( $_REQUEST['fSubmitBio'] ) ) {
 	$gQueryUser->store( $_REQUEST );
+	$url = $gQueryUser->getDisplayUrl( $gQueryUser->mInfo['login'] );
+	header( "Location: ".$url );
 } elseif( isset( $_REQUEST['fSubmitDeletePortait'] ) ) {
 	$gQueryUser->purgePortrait();
 } elseif( isset( $_REQUEST['fSubmitDeleteAvatar'] ) ) {
