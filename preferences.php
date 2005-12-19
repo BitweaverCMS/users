a8b803736fa71e33176a2b9118a25adbfe108caf
Max Kremmel <xing@synapse.plus.com>
2005-12-19 08:30:56 +0000

    don't allow guests to visit user preference page

--- preferences.php
+++ preferences.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/preferences.php,v 1.2.2.6 2005/12/18 20:25:48 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/preferences.php,v 1.2.2.7 2005/12/19 08:30:56 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: preferences.php,v 1.2.2.6 2005/12/18 20:25:48 lsces Exp $
+ * $Id: preferences.php,v 1.2.2.7 2005/12/19 08:30:56 squareing Exp $
  * @package users
  * @subpackage functions
  */
@@ -25,16 +25,14 @@ if( $gBitSystem->isPackageActive( 'blogs' ) ) {
 	include_once( BLOGS_PKG_PATH.'BitBlog.php' );
 }
 // User preferences screen
-if ($feature_userPreferences != 'y') {
-	$gBitSmarty->assign('msg', tra("This feature is disabled").": feature_userPreferences");
+$gBitSystem->verifyFeature( 'feature_userPreferences' );
+
+if( !$gBitUser->isRegistered() ) {
+	$gBitSmarty->assign( 'msg', tra( "You are not logged in" ) );
 	$gBitSystem->display( 'error.tpl' );
 	die;
 }
-if (empty($gBitUser->mUserId)) {
-	$gBitSmarty->assign('msg', tra("You are not logged in"));
-	$gBitSystem->display( 'error.tpl' );
-	die;
-}
+
 if( !empty( $_REQUEST["view_user"] ) && $_REQUEST["view_user"] <> $gBitUser->mUserId) {
 	$gBitSystem->verifyPermission( 'bit_p_admin_users' );
 	$editUser = new BitUser( $_REQUEST["view_user"] );
@@ -43,6 +41,7 @@ if( !empty( $_REQUEST["view_user"] ) && $_REQUEST["view_user"] <> $gBitUser->mUs
 } else {
 	$editUser = &$gBitUser;
 }
+
 global $gQueryUserId;
 $gQueryUserId = &$editUser->mUserId;
 
