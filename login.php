100122ca0936e3334fdd7f123d677b8f30bce220
Max Kremmel <xing@synapse.plus.com>
2006-02-17 18:06:45 +0000

    fix empty index

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/login.php,v 1.6 2006/02/17 13:57:55 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/login.php,v 1.7 2006/02/17 18:06:45 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: login.php,v 1.6 2006/02/17 13:57:55 squareing Exp $
+ * $Id: login.php,v 1.7 2006/02/17 18:06:45 squareing Exp $
  * @package users
  * @subpackage functions
  */
@@ -21,7 +21,7 @@ include_once ("../bit_setup_inc.php");
 if( !empty( $_SERVER['HTTP_REFERER'] ) && !strpos( $_SERVER['HTTP_REFERER'], 'login.php' )  && !strpos( $_SERVER['HTTP_REFERER'], 'register.php' ) ) {
 	$from = parse_url( $_SERVER['HTTP_REFERER'] );
 	if( $from['host'] == $_SERVER['SERVER_NAME'] ) {
-		$_SESSION['loginfrom'] = $from['path'].'?'.$from['query'];
+		$_SESSION['loginfrom'] = $from['path'].'?'.( !empty( $from['query'] ) ? $from['query'] : '' );
 	}
 }
 
