041b6fcae24fcb56d301f3711d9b08f5c5264fac
Max Kremmel <xing@synapse.plus.com>
2006-04-23 16:51:51 +0000

    clean up code, make output image nicer, use some colours, try to use image formats other than jpg if available

--- random_num_img.php
+++ random_num_img.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/Attic/random_num_img.php,v 1.2 2005/06/28 07:46:23 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/Attic/random_num_img.php,v 1.3 2006/04/23 16:51:51 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: random_num_img.php,v 1.2 2005/06/28 07:46:23 spiderr Exp $
+ * $Id: random_num_img.php,v 1.3 2006/04/23 16:51:51 squareing Exp $
  * @package users
  * @subpackage functions
  */
@@ -16,25 +16,43 @@
 /**
  * required setup
  */
-//session_start();
-// hmm. to many session tweaks in setup_smarty ... we need to call this
+// hmm. too many session tweaks in setup_smarty ... we need to call this
 require_once( '../bit_setup_inc.php' );
-//random_number.php
-$img_number = imagecreate(95, 30);
-$white = imagecolorallocate($img_number, 255, 255, 255);
-$black = imagecolorallocate($img_number, 0, 0, 0);
-$grey_shade = imagecolorallocate($img_number, 204, 204, 204);
-imagefill($img_number, 0, 0, $grey_shade);
-srand (time());
-$number = get_random();
+
+// dimensions
+$width  = 140;
+$height = 35;
+
+$img = @imagecreate( $width, $height ) or die( "The GD image library doesn't seem to be available or doesn't have JPG support." );
+
+// colours
+$text[] = imagecolorallocate( $img, 0  , 0  , 128 );
+$text[] = imagecolorallocate( $img, 0  , 128, 0   );
+$text[] = imagecolorallocate( $img, 128, 0  , 0   );
+$text[] = imagecolorallocate( $img, 0  , 128, 128 );
+$text[] = imagecolorallocate( $img, 128, 128, 0   );
+$text[] = imagecolorallocate( $img, 128, 0  , 128 );
+$border = imagecolorallocate( $img, 0  , 0  , 0   );
+$bg     = imagecolorallocate( $img, 230, 240, 250 );
+
+imagefill( $img, 0, 0, $bg );
+imagerectangle( $img, 1, 1, $width - 1, $height - 1, $border );
+srand( time() );
+$number = rand( 10000, 99999 );
 $_SESSION['random_number'] = $number;
-for ($i = 0; $i < 5; $i++) {
-	Imagestring($img_number, 5, 10 + 17 * $i + rand(0, 2), 1 + rand(0, 10), substr($number, $i, 1), $black);
+for( $i = 0; $i < 5; $i++ ) {
+	imagestring( $img, 10, ( $width / 6 ) - 5 + ( $width / 6 ) * $i + rand( 0, 2 ), 2 + rand( 0, $height - 22 ), substr( $number, $i, 1 ), $text[rand( 0, count( $text ) - 1 )] );
 }
-header ("Content-type: image/jpeg");
-imagejpeg ($img_number);
-function get_random() {
-	# return a 5 digit value
-	return rand(10000, 99999);
+
+$gd = gd_info();
+if( $gd["PNG Support"] ) {
+	header( "Content-type: image/png" );
+	imagepng( $img );
+} elseif( $gd["GIF Create Support"] ) {
+	header( "Content-type: image/gif" );
+	imagegif( $img );
+} else {
+	header( "Content-type: image/jpeg" );
+	imagejpeg( $img );
 }
 ?>
