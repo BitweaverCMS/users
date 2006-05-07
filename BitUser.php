87761f411815df054f91283fbd19309726d60cfd
Sylvie Greverend <sylvieg@users.sourceforge.net>
2006-05-07 20:33:25 +0000

    when the user displayed in the 'created by...' has been deleted, to be able to go on

--- BitUser.php
+++ BitUser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.67 2006/05/07 16:28:40 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.68 2006/05/07 20:33:25 sylvieg Exp $
  *
  * Lib for user administration, groups and permissions
  * This lib uses pear so the constructor requieres
@@ -12,7 +12,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitUser.php,v 1.67 2006/05/07 16:28:40 spiderr Exp $
+ * $Id: BitUser.php,v 1.68 2006/05/07 20:33:25 sylvieg Exp $
  * @package users
  */
 
@@ -40,7 +40,7 @@ define("ACCOUNT_DISABLED", -6);
  * Class that holds all information for a given user
  *
  * @author   spider <spider@steelsun.com>
- * @version  $Revision: 1.67 $
+ * @version  $Revision: 1.68 $
  * @package  users
  * @subpackage  BitUser
  */
@@ -1537,7 +1537,7 @@ echo "userAuthPresent: $userAuthPresent<br>";
 	function getDisplayName($pUseLink = FALSE, $pHash=NULL) {
 		global $gBitSystem, $gBitUser;
 		$ret = NULL;
-		if( empty( $pHash ) && !empty( $this->mInfo ) ) {
+		if( empty( $pHash ) && !empty($this) && !empty( $this->mInfo ) ) {
 			$pHash = &$this->mInfo;
 		}
 		if( !empty( $pHash ) ) {
