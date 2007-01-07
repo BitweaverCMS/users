bd691f3b6c9aa6edaf800e46f0f9063b6576858c
Max Kremmel <xing@synapse.plus.com>
2007-01-07 22:31:09 +0000

    allow users to set time value

--- modules/mod_online_users.php
+++ modules/mod_online_users.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_online_users.php,v 1.5 2006/07/28 19:34:19 bitweaver Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_online_users.php,v 1.6 2007/01/07 22:31:09 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,12 +8,12 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_online_users.php,v 1.5 2006/07/28 19:34:19 bitweaver Exp $
+ * $Id: mod_online_users.php,v 1.6 2007/01/07 22:31:09 squareing Exp $
  * @package users
  * @subpackage modules
  */
- global $gBitUser;
-$listHash['last_get'] = 3600;
+ global $gBitUser, $module_params;
+$listHash['last_get'] = !empty( $module_params['time_buffer'] ) ? $module_params['time_buffer'] : 900;
 $online_users = $gBitUser->getUserActivity( $listHash );
 $gBitSmarty->assign('online_users', $online_users);
 $gBitSmarty->assign('logged_users', count( $online_users ) );
