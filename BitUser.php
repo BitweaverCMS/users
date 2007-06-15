cd44e3a52599baad9e9b8f1930d89452bccca7ec
Lester Caine <lester@lsces.co.uk>
2007-06-15 20:33:00 +0000

    Fix error message

--- BitUser.php
+++ BitUser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.140 2007/06/14 08:24:25 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.141 2007/06/15 20:33:00 lsces Exp $
  *
  * Lib for user administration, groups and permissions
  * This lib uses pear so the constructor requieres
@@ -12,7 +12,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitUser.php,v 1.140 2007/06/14 08:24:25 squareing Exp $
+ * $Id: BitUser.php,v 1.141 2007/06/15 20:33:00 lsces Exp $
  * @package users
  */
 
@@ -40,7 +40,7 @@ define("ACCOUNT_DISABLED", -6);
  * Class that holds all information for a given user
  *
  * @author   spider <spider@steelsun.com>
- * @version  $Revision: 1.140 $
+ * @version  $Revision: 1.141 $
  * @package  users
  * @subpackage  BitUser
  */
@@ -1212,7 +1212,7 @@ class BitUser extends LibertyAttachable {
 					$this->storeAvatar( $pStorageHash );
 				}
 			} else {
-				$this->mErrors['file'] = 'File '.$pStorageHash['name'].' could not be stored.';
+				$this->mErrors['file'] = 'File '.$pStorageHash['upload']['name'].' could not be stored.';
 			}
 		}
 		return( count( $this->mErrors ) == 0 );
