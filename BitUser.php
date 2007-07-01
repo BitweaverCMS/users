6de5748848eb8556f124a303d633fbc32772d21f
Lester Caine <lester@lsces.co.uk>
2007-07-01 21:25:09 +0000

    Remove extranous root path reference

--- BitUser.php
+++ BitUser.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.147 2007/06/29 09:37:07 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/BitUser.php,v 1.148 2007/07/01 21:25:09 lsces Exp $
  *
  * Lib for user administration, groups and permissions
  * This lib uses pear so the constructor requieres
@@ -12,7 +12,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitUser.php,v 1.147 2007/06/29 09:37:07 lsces Exp $
+ * $Id: BitUser.php,v 1.148 2007/07/01 21:25:09 lsces Exp $
  * @package users
  */
 
@@ -40,7 +40,7 @@ define("ACCOUNT_DISABLED", -6);
  * Class that holds all information for a given user
  *
  * @author   spider <spider@steelsun.com>
- * @version  $Revision: 1.147 $
+ * @version  $Revision: 1.148 $
  * @package  users
  * @subpackage  BitUser
  */
@@ -1612,7 +1612,7 @@ class BitUser extends LibertyAttachable {
 		global $gBitSystem;
 		while( $res = $result->fetchRow() ) {
 			if( !empty($res['avatar_storage_path'] ) ) {
-				$res['avatar_url'] = BIT_ROOT_URL.$res['avatar_storage_path'];
+				$res['avatar_url'] = $res['avatar_storage_path'];
 				$res['thumbnail_url'] = liberty_fetch_thumbnail_url( $res['avatar_url'], 'small' );
 			}
 			$res["groups"] = $this->getGroups( $res['user_id'] );
