d9b5bb8b277fc1c46889f522312b491ddf19185c
Max Kremmel <xing@synapse.plus.com>
2007-11-09 10:54:19 +0000

    just some code cleanup and indent using tabs

--- change_password.php
+++ change_password.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/change_password.php,v 1.11 2007/11/09 10:29:39 joasch Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/change_password.php,v 1.12 2007/11/09 10:54:19 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: change_password.php,v 1.11 2007/11/09 10:29:39 joasch Exp $
+ * $Id: change_password.php,v 1.12 2007/11/09 10:54:19 squareing Exp $
  * @package users
  * @subpackage functions
  */
@@ -17,25 +17,28 @@
  * required setup
  */
 require_once( '../bit_setup_inc.php' );
-if (!isset($_REQUEST['login']))
+if( !isset( $_REQUEST['login'] )) {
 	$_REQUEST['login'] = '';
-if (!isset($_REQUEST["oldpass"]))
+}
+if( !isset( $_REQUEST["oldpass"] )) {
 	$_REQUEST["oldpass"] = '';
-if (!isset($_REQUEST["provpass"]))
+}
+if( !isset( $_REQUEST["provpass"] )) {
 	$_REQUEST["provpass"] = '';
+}
 
-$gBitSmarty->assign('login', $_REQUEST['login']);
-$gBitSmarty->assign('oldpass', $_REQUEST["oldpass"]);
-$gBitSmarty->assign('provpass', $_REQUEST["provpass"]);
-if (isset($_REQUEST["change"])) {
+$gBitSmarty->assign( 'login', $_REQUEST['login'] );
+$gBitSmarty->assign( 'oldpass', $_REQUEST["oldpass"] );
+$gBitSmarty->assign( 'provpass', $_REQUEST["provpass"] );
 
-	$userInfo = $gBitUser->getUserInfo( array( 'user_id' => $_REQUEST['user_id'] ) );
+if( isset( $_REQUEST["change"] )) {
+	$userInfo = $gBitUser->getUserInfo( array( 'user_id' => $_REQUEST['user_id'] ));
 
-	if ($_REQUEST["pass"] == $_REQUEST["oldpass"]) {
-		$gBitSystem->fatalError( tra("You can not use the same password again") );
+	if( $_REQUEST["pass"] == $_REQUEST["oldpass"] ) {
+		$gBitSystem->fatalError( tra( "You can not use the same password again" ));
 	}
-	
-    if( $passswordError = $gBitUser->verifyPasswordFormat( $_REQUEST["pass"], $_REQUEST["pass2"] ) ) {
+
+	if( $passswordError = $gBitUser->verifyPasswordFormat( $_REQUEST["pass"], $_REQUEST["pass2"] )) {
 		$gBitSystem->fatalError( tra( $passswordError ));
 	}
 
@@ -43,16 +46,16 @@ if (isset($_REQUEST["change"])) {
 	if( !empty( $_REQUEST["provpass"] ) ) {
 		if( $validated = $gBitUser->confirmRegistration( $userInfo['user_id'], $_REQUEST["provpass"] ) ) {
 			if( $gBitSystem->isFeatureActive( 'send_welcome_email' ) ) {
-	            $siteName = $gBitSystem->getConfig('site_title', $_SERVER['HTTP_HOST'] );
-	            $gBitSmarty->assign('siteName',$_SERVER["SERVER_NAME"]);
-	            $gBitSmarty->assign('mail_site',$_SERVER["SERVER_NAME"]);
-	            $gBitSmarty->assign('mail_user',$userInfo['login']);
+				$siteName = $gBitSystem->getConfig( 'site_title', $_SERVER['HTTP_HOST'] );
 				// Send the welcome mail
+				$gBitSmarty->assign( 'siteName', $_SERVER["SERVER_NAME"] );
+				$gBitSmarty->assign( 'mail_site', $_SERVER["SERVER_NAME"] );
+				$gBitSmarty->assign( 'mail_user', $userInfo['login'] );
 				$gBitSmarty->assign( 'mailPassword',$_REQUEST['pass'] );
 				$gBitSmarty->assign( 'mailEmail',$validated['email'] );
 				$mail_data = $gBitSmarty->fetch('bitpackage:users/welcome_mail.tpl');
 				mail($validated["email"], tra( 'Welcome to' ).' '.$siteName,$mail_data,"From: ".$gBitSystem->getConfig('site_sender_email')."\r\nContent-type: text/plain;charset=utf-8\r\n");
- 			}
+			}
 		} else	{
 				$gBitSystem->fatalError( tra("Password reset request is invalid or has expired") );
 		}
@@ -67,7 +70,7 @@ if (isset($_REQUEST["change"])) {
 		$url = $gBitUser->login( $userInfo['login'], $_REQUEST["pass"] );
 	}
 
-	header ( "location: ".$url );
+	bit_redirect( $url );
 }
 
 // Display the template
