77dd898ab94b2bada11004d42c6f0d561e1d643f
Christian Fowler <spider@viovio.com>
2008-02-19 22:03:12 +0000

    set 410 'GONE' http status for This confirmation link is no longer valid.

--- confirm.php
+++ confirm.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/confirm.php,v 1.5 2006/09/12 19:26:48 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/confirm.php,v 1.6 2008/02/19 22:03:12 spiderr Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: confirm.php,v 1.5 2006/09/12 19:26:48 spiderr Exp $
+ * $Id: confirm.php,v 1.6 2008/02/19 22:03:12 spiderr Exp $
  * @package users
  * @subpackage functions
  */
@@ -26,6 +26,7 @@ if( !empty( $userId ) && !empty( $provPass ) && $userInfo = $gBitUser->confirmRe
 	$gBitSmarty->assign_by_ref( 'userInfo', $userInfo );
 	$gBitSystem->display( 'bitpackage:users/change_password.tpl', 'Confrim Password Change' );
 } else {
+	$gBitSystem->setHttpStatus( 410 );
 	$gBitSystem->fatalError( tra("This confirmation link is no longer valid.  Please Login or <a href=\"".USERS_PKG_URL."remind_password.php\">request a new password change</a>") );
 }
 ?>
