54870d05074a71984122b7126ba179b5184bba02
Max Kremmel <xing@synapse.plus.com>
2008-05-17 14:43:11 +0000

    trailing whitespace

--- admin/users_import.php
+++ admin/users_import.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_users/admin/users_import.php,v 1.1 2007/11/15 19:27:20 joasch Exp $
+// $Header: /cvsroot/bitweaver/_bit_users/admin/users_import.php,v 1.2 2008/05/17 14:43:11 squareing Exp $
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -16,20 +16,20 @@ if( isset( $_REQUEST["batchimport"])) {
 
 	if( $_FILES['csvlist']['size'] && is_uploaded_file($_FILES['csvlist']['tmp_name'] ) ) {
 		global $gBitSmarty, $gBitUser, $gBitSystem;
-	
+
 		// get the delimiter if it's set - use comma if it not
 		$delimiter = !empty( $_REQUEST['delimiter'] ) ? $_REQUEST['delimiter'] : ",";
 		$fname = $_FILES['csvlist']['tmp_name'];
 		$fhandle = fopen( $fname, "r" );
-	
+
 		//Get the field names
 		$fields = fgetcsv( $fhandle, 1000, $delimiter );
-	
+
 		// is the file a valid CSV file?
 		if( empty( $fields[0] ) ) {
 			$gBitSystem->fatalError( tra( "The file is not a CSV file or has not a correct syntax" ));
 		}
-	
+
 		//now load the users in a table
 		while( !feof( $fhandle ) ) {
 			if( $data = fgetcsv( $fhandle, 1000, $delimiter ) ) {
@@ -40,7 +40,7 @@ if( isset( $_REQUEST["batchimport"])) {
 			}
 		}
 		fclose( $fhandle );
-	
+
 		// were there any users in the list?
 		if( !is_array( $userRecords ) ) {
 			$gBitSystem->fatalError( tra( "No records were found. Check the file please!" ));
@@ -69,7 +69,7 @@ if( isset( $_REQUEST["batchimport"])) {
 					$logHash['action_log']['title'] = $userRecord['login'];
 					$newUser->storeActionLog( $logHash );
 				}
-	
+
 				$added++;
 			} else {
 				$discarded[$i] = implode( ',', $newUser->mErrors );
@@ -77,7 +77,7 @@ if( isset( $_REQUEST["batchimport"])) {
 			unset( $newUser );
 			$i++;
 		}
-	
+
 		$gBitSmarty->assign( 'added', $added );
 		if( @is_array( $discarded ) ) {
 			$gBitSmarty->assign( 'discarded', count( $discarded ) );
