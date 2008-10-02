75aa8e86ed49abc060c053a84e26b2dd849aa802
Nick Palmer <nick@sluggardy.net>
2008-10-02 14:52:04 +0000

    Use empty instead of ! in case it isn't set yet.

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_users/bit_setup_inc.php,v 1.46 2008/10/02 14:45:39 nickpalmer Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_users/bit_setup_inc.php,v 1.47 2008/10/02 14:52:04 nickpalmer Exp $
  * @package users
  */
 global $gBitDbType, $gBitDbHost, $gBitDbUser, $gBitDbPassword, $gBitDbName, $gBitThemes;
@@ -59,7 +59,7 @@ if( $gBitSystem->isFeatureActive( 'users_remember_me' )) {
 // just use a simple COOKIE (unique random string) that is linked to the users_cnxn table.
 // This way, nuking rows in the users_cnxn table can log people out and is much more reliable than SESSIONS
 global $gShellScript;
-if(!$gShellScript) {
+if( empty( $gShellScript ) ) {
 	session_start();
 }
 $cookie_site = strtolower( ereg_replace( "[^a-zA-Z0-9]", "", $gBitSystem->getConfig( 'site_title', 'bitweaver' )));
