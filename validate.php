a34abe3b75d4f3624fff1f7acaf98d7f0db8331f
Max Kremmel <xing@synapse.plus.com>
2008-10-03 14:50:18 +0000

    add hack to allow users to log in when the installer force is called

--- validate.php
+++ validate.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/validate.php,v 1.18 2008/07/22 11:00:34 wolff_borg Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/validate.php,v 1.19 2008/10/03 14:50:18 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,16 +8,21 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: validate.php,v 1.18 2008/07/22 11:00:34 wolff_borg Exp $
+ * $Id: validate.php,v 1.19 2008/10/03 14:50:18 squareing Exp $
  * @package users
  * @subpackage functions
  */
 $bypass_siteclose_check = 'y';
 
+// this is a dirty hack to allow admins to log in when we require a visit to the installer
+// used in /bit_setup_inc.php - xing - Friday Oct 03, 2008   16:44:48 CEST
+define( 'LOGIN_VALIDATE', TRUE );
+
 /**
  * required setup
  */
 require_once( '../bit_setup_inc.php' );
+
 global $gBitSystem;
 
 //Remember where user is logging in from and send them back later; using session variable for those of us who use WebISO services
@@ -56,10 +61,10 @@ if (!preg_match('/^\w+:\/{2}/', $url)) {
 }
 
 // but if we came from a login page, let's go home (except if we got an error when login in)
-if( (strpos( $url, 'login.php?' ) || strpos( $url, 'remind_password.php' )) && strpos( $url, 'login.php?error=') == -1) {
+if(( strpos( $url, 'login.php?' ) || strpos( $url, 'remind_password.php' )) && strpos( $url, 'login.php?error=' ) == -1 ) {
 	$url = $gBitSystem->getDefaultPage();
 }
 
-header('Location: ' . $url);
+bit_redirect( $url );
 exit;
 ?>
