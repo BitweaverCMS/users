448834da5e2dd2163f1eaa85ce7e6ae748c0ee0c
Max Kremmel <xing@synapse.plus.com>
2008-10-16 08:49:54 +0000

    clean up code and get pagination working

--- admin/user_activity.php
+++ admin/user_activity.php
@@ -1,29 +1,27 @@
 <?php
 
-	require_once( '../../bit_setup_inc.php' );
-	
-	$listHash = array();
-	
-	if( @BitBase::verifyId( $_REQUEST['user_id'] ) ) {
-		$listHash['user_id'] = $_REQUEST['user_id'];
-	}
+require_once( '../../bit_setup_inc.php' );
 
-	if( !empty( $_REQUEST['user_agent'] ) ) {
-		$listHash['user_agent'] = $_REQUEST['user_agent'];
-	}
+$listHash = $_REQUEST;
 
-	if( !empty( $_REQUEST['ip'] ) ) {
-		$listHash['ip'] = $_REQUEST['ip'];
-	}
+if( @BitBase::verifyId( $_REQUEST['user_id'] ) ) {
+	$listHash['user_id'] = $_REQUEST['user_id'];
+}
 
-	if( @BitBase::verifyId( $_REQUEST['user_id'] ) ) {
-		$listHash['user_id'] = $_REQUEST['user_id'];
-	}
+if( !empty( $_REQUEST['user_agent'] ) ) {
+	$listHash['user_agent'] = $_REQUEST['user_agent'];
+}
 
-	$gBitSmarty->assign_by_ref( 'userActivity', $gBitUser->getUserActivity( $listHash ) );
+if( !empty( $_REQUEST['ip'] ) ) {
+	$listHash['ip'] = $_REQUEST['ip'];
+}
 
-	$gBitSmarty->assign_by_ref( 'listInfo', $listHash['listInfo'] );
+if( @BitBase::verifyId( $_REQUEST['user_id'] ) ) {
+	$listHash['user_id'] = $_REQUEST['user_id'];
+}
 
-	$gBitSystem->display( 'bitpackage:users/user_activity.tpl', 'User Activity' , array( 'display_mode' => 'admin' ));
+$gBitSmarty->assign_by_ref( 'userActivity', $gBitUser->getUserActivity( $listHash ));
+$gBitSmarty->assign_by_ref( 'listInfo', $listHash['listInfo'] );
+$gBitSystem->display( 'bitpackage:users/user_activity.tpl', 'User Activity' , array( 'display_mode' => 'admin' ));
 
 ?>
