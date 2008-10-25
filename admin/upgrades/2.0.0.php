555266ad4dbbccf3e96a91c716e310d808052231
Max Kremmel <xing@synapse.plus.com>
2008-10-25 08:30:51 +0000

    more work towards package upgrades. things seem to working quite smoothly now.

--- admin/upgrades/2.0.0.php
+++ admin/upgrades/2.0.0.php
@@ -4,7 +4,7 @@ global $gBitInstaller;
 $infoHash = array(
 	'package'      => USERS_PKG_NAME,
 	'version'      => str_replace( '.php', '', basename( __FILE__ )),
-	'description'  => "This upgrade removes an unused table from the database.",
+	'description'  => "Remove the unused <em>users_semaphores</em> table from your database. If you need a semaphores feature, there is a <a class='external' href='http://www.bitweaver.org/wiki/SemaphorePackage'>SemaphorePackage</a> now.",
 	'post_upgrade' => NULL,
 );
 
