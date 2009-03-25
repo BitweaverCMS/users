c4671b52164f08fe0fbab8dd1cd5b5512f653072
Max Kremmel <xing@synapse.plus.com>
2009-03-25 08:28:11 +0000

    fix all backticks and remove duplicate updates

--- admin/upgrades/2.1.1.php
+++ admin/upgrades/2.1.1.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_users/admin/upgrades/2.1.1.php,v 1.1 2009/03/25 02:32:50 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_users/admin/upgrades/2.1.1.php,v 1.2 2009/03/25 08:28:11 squareing Exp $
  */
 global $gBitInstaller;
 
@@ -20,12 +20,9 @@ $gBitInstaller->registerPackageUpgrade( $infoHash, array(
 array( 'QUERY' =>
 	// postgres > 8.2 needs to have the type cast
 	array(
-		'PGSQL' => array(	"ALTER TABLE `".BIT_DB_PREFIX."users_cnxn ALTER `ip` TYPE VARCHAR(39)" ,
-		),
-		'OCI' => array(	"ALTER TABLE `".BIT_DB_PREFIX."users_cnxn MODIFY (`ip` TYPE VARCHAR2(39))" ,
-		),
-		'MYSQL' => array(	"ALTER TABLE `".BIT_DB_PREFIX."users_cnxn MODIFY `ip` TYPE VARCHAR(39)" ,
-		),
+		'PGSQL' => array( "ALTER TABLE `".BIT_DB_PREFIX."users_cnxn` ALTER `ip` TYPE VARCHAR(39)" ,),
+		'OCI'   => array( "ALTER TABLE `".BIT_DB_PREFIX."users_cnxn` MODIFY (`ip` TYPE VARCHAR2(39))" ,),
+		'MYSQL' => array( "ALTER TABLE `".BIT_DB_PREFIX."users_cnxn` MODIFY `ip` TYPE VARCHAR(39)" ,),
 	),
 ),
 
