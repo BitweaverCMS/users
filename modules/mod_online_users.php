7fed9d9d7baaca3819c836be6562d5e26af9159f
Tekimaki <tekimaki_admin@users.sourceforge.net>
2009-04-13 21:25:28 +0000

    flag required to limit to only those online

--- modules/mod_online_users.php
+++ modules/mod_online_users.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_online_users.php,v 1.6 2007/01/07 22:31:09 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_online_users.php,v 1.7 2009/04/13 21:25:28 tekimaki_admin Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,11 +8,12 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_online_users.php,v 1.6 2007/01/07 22:31:09 squareing Exp $
+ * $Id: mod_online_users.php,v 1.7 2009/04/13 21:25:28 tekimaki_admin Exp $
  * @package users
  * @subpackage modules
  */
  global $gBitUser, $module_params;
+$listHash['online' ] = true; 
 $listHash['last_get'] = !empty( $module_params['time_buffer'] ) ? $module_params['time_buffer'] : 900;
 $online_users = $gBitUser->getUserActivity( $listHash );
 $gBitSmarty->assign('online_users', $online_users);
