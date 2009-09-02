09864eb486824072a01315404f242dd48cc56f51
Christian Fowler <spider@viovio.com>
2009-09-02 18:21:58 +0000

    add gBitSystem->getConfig( 'user_class', 'BitPermUser' )

--- modules/mod_user_profile.php
+++ modules/mod_user_profile.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_user_profile.php,v 1.7 2007/06/14 19:26:19 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_user_profile.php,v 1.8 2009/09/02 18:21:58 spiderr Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,19 +8,20 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_user_profile.php,v 1.7 2007/06/14 19:26:19 squareing Exp $
+ * $Id: mod_user_profile.php,v 1.8 2009/09/02 18:21:58 spiderr Exp $
  * @package users
  * @subpackage modules
  */
 global $gQueryUser, $gBitUser, $gBitSmarty, $moduleParams;
 extract( $moduleParams );
 
+$userClass = $gBitSystem->getConfig( 'user_class', 'BitPermUser' );
 if( !empty( $module_params['user_id'] )) {
-	$user = new BitUser( $module_params['user_id'] );
+	$user = $userClass( $module_params['user_id'] );
 	$user->load();
 	$gBitSmarty->assign_by_ref( 'userInfo', $user->mInfo );
 } elseif( !empty( $module_params['login'] )) {
-	$user = new BitUser();
+	$user = $userClass();
 	$user->load(null,$module_params['login']);
 	$gBitSmarty->assign_by_ref( 'userInfo', $user->mInfo);
 } elseif( !empty( $gQueryUser->mInfo )) {
