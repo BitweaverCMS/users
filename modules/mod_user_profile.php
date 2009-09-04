d917a4d07af4e6699785d691a96aca0a72d1450d
Christian Fowler <spider@viovio.com>
2009-09-04 15:30:05 +0000

    force portrait_url to small... for now. will add module_param shortly

--- modules/mod_user_profile.php
+++ modules/mod_user_profile.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_user_profile.php,v 1.9 2009/09/03 16:28:26 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_users/modules/mod_user_profile.php,v 1.10 2009/09/04 15:30:05 spiderr Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_user_profile.php,v 1.9 2009/09/03 16:28:26 spiderr Exp $
+ * $Id: mod_user_profile.php,v 1.10 2009/09/04 15:30:05 spiderr Exp $
  * @package users
  * @subpackage modules
  */
@@ -33,6 +33,8 @@ if( !empty( $module_params['user_id'] )) {
 	$userInfo = &$gBitUser->mInfo;
 	$userPrefs = &$gBitUser->mPrefs;
 }
+$userInfo['portrait_url']  = liberty_fetch_thumbnail_url( array( 'storage_path' => $userInfo['portrait_storage_path'], 'size' => 'small', 'mime_image' => FALSE ));
+
 $gBitSmarty->assign_by_ref( 'userInfo', $userInfo );
 $gBitSmarty->assign_by_ref( 'userPrefs', $userPrefs );
 ?>
