cca168af082856ed152acddff4224bd3d8eaef1f
Christian Fowler <spider@viovio.com>
2009-10-07 19:50:05 +0000

    add start_user_id GET paramter to allow offset, timestamp output

--- admin/verify_emails.php
+++ admin/verify_emails.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_users/admin/verify_emails.php,v 1.7 2009/10/01 14:17:06 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_users/admin/verify_emails.php,v 1.8 2009/10/07 19:50:05 spiderr Exp $
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
@@ -9,11 +9,18 @@ require_once( '../../bit_setup_inc.php' );
 	
 	$gBitUser->verifyTicket();
 
-	$selectSql = 'SELECT uu.user_id,uu.email  FROM users_users uu WHERE user_id NOT IN (SELECT user_id FROM users_groups_map WHERE group_id = ?) ORDER BY uu.user_id';
-	$users     = $gBitDb->getAssoc($selectSql, array( $gBitSystem->getConfig('users_validate_email_group') ) );
+	$whereSql = '';
+	$bindVars = array( $gBitSystem->getConfig('users_validate_email_group') );
+	if( !empty( $_REQUEST['start_user_id'] ) ) {
+		$whereSql = " AND user_id>?";
+		$bindVars[] = $_REQUEST['start_user_id'];
+	}
+
+	$selectSql = "SELECT uu.user_id,uu.email  FROM users_users uu WHERE user_id NOT IN (SELECT user_id FROM users_groups_map WHERE group_id = ?) $whereSql ORDER BY uu.user_id";
+	$users     = $gBitDb->getAssoc($selectSql, $bindVars );
 	$errors;
 	foreach ( $users as $id=>$email ){
-		print "Verifying $email ( $id ) .... ";
+		print date( "Y-m-d H:i:s" )." Verifying $email ( $id ) .... ";
 		flush();
 		$emailStatus = $gBitUser->verifyMx($email,$errors);
 		if( $emailStatus === true){
