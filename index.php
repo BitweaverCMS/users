71ebbd84317edc55273c95ae971ae67751acc593
spiderr <spider@viovio.com>
2013-03-13 01:48:28 -0400

    add rel=canonical to user homepage

--- index.php
+++ index.php
@@ -38,6 +38,8 @@ if( !empty( $_REQUEST['home'] ) && $gQueryUser->isValid() && (( $gBitUser->hasPe
 		require( STARS_PKG_PATH."templates/user_ratings.php" );
 	}
 
+	$gBitSystem->setCanonicalLink( $gQueryUser->getDisplayUri() );
+
 	if( $gQueryUser->canCustomizeTheme() ) {
 		$userHomeStyle = $gQueryUser->getPreference( 'theme' );
 		if( !empty( $userHomeStyle )) {
