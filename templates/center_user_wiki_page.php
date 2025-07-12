37d2d57f24fb041ec0abfc58651564e7dfcd4e31
spider@dav <spiderr@bitweaver.org>
2025-07-12 08:42:32 -0400

    check for $_template in center_user_wiki_page

--- templates/center_user_wiki_page.php
+++ templates/center_user_wiki_page.php
@@ -1,9 +1,9 @@
 <?php
 
-global $gQueryUser;
+global $gQueryUser, $_template;
 
 include_once( USERS_PKG_INCLUDE_PATH.'lookup_user_inc.php' );
 
-if( $gQueryUser && $gQueryUser->isValid() ) {
+if( $_template && $gQueryUser && $gQueryUser->isValid() ) {
 	$_template->tpl_vars['gQueryUser'] = new Smarty_variable( $gQueryUser );
 }
